2a776a9a25c83c265e0cf10640f81d2399b30d87
peter-janderks@hotmail.com <peter-jan.derks@student.auc.nl>
2019-04-04 21:55:01 +0200

    testing metropolis for a second function but not working yet

--- metropolis_algorithm_test.py
+++ metropolis_algorithm_test.py
@@ -4,20 +4,30 @@ from scipy import integrate
 from metropolis_algorithm import metropolis
 
 
-def example_1():
-    def trial_wave_function(R):
-        return(np.exp(- 0.5 * R**2))
-
+def example_1d_oscillator():
     f= lambda x:np.exp(-2*0.5*x**2)
-    int = integrate.quad(f, -1000, 1000)
-    p_r = lambda r:np.exp(-2*0.5*r**2)/int[0]
+    integral = integrate.quad(f, -1000, 1000)
+    p_r = lambda r:np.exp(-2*0.5*r**2)/integral[0]
 
-    density = metropolis(trial_wave_function,100000)
+    density = metropolis(f,100000)
     plt.figure()
     plt.title(r'$\rho(x) = \frac{e^{-0.5\alpha x^2}}{\int e^{-\alpha x^2} dx}$') 
     count, bins, ignored = plt.hist(density[1000:], 30, density=True)
     plt.plot(bins, p_r(bins),linewidth=2, color='r')
 
+def example_helium():
+    g = lambda x:np.exp(-2)*np.exp(-4)*np.exp(2/(2*(1+x*2)))
+    int = integrate.quad(g, -1000, 1000)
+    p_r = lambda r:np.exp(-2*0.5*r**2)/int[0]
+
+    density = metropolis(g,100000)
+    plt.figure()
+    plt.title('helium')
+    count, bins, ignored = plt.hist(density[1000:], 30, density=True)
+    plt.plot(bins, p_r(bins),linewidth=2, color='r')
+
 if __name__ == '__main__':
-    example_1()
+    example_1d_oscillator()
+    example_helium()
     plt.show()
+
