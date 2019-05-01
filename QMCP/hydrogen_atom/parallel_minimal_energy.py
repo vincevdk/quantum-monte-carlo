51656caf389c1f96b8dd15c81ecd645899cd0b91
peter-janderks@hotmail.com <peter-jan.derks@student.auc.nl>
2019-05-01 12:29:22 +0200

    added parallel minimal energy finder

--- QMCP/hydrogen_atom/parallel_minimal_energy.py
+++ QMCP/hydrogen_atom/parallel_minimal_energy.py
@@ -1,16 +1,18 @@
 import time
 import numpy as np
 import matplotlib.pyplot as plt
-from QMCP.functions import minimization_alpha
+from QMCP.functions import parallel_minimization_alpha
 from QMCP.hydrogen_atom import Hydrogen_atom
 
 start = time.time()
-alpha = minimization_alpha(Hydrogen_atom(3))
-steps = np.linspace(0,len(alpha),len(alpha))
-print("If I'm not running parallel it takes" + str(time.time() - start))
+
+alpha = parallel_minimization_alpha(Hydrogen_atom(3))
+steps = np.linspace(0,len(alpha)-1,len(alpha))
+print("My work is finished, I took " + str(time.time() - start))
 
 print(steps,'steps')
 plt.title('minimization of alpha')
 plt.plot(steps,alpha, 'ro')
 
 plt.show()
+
