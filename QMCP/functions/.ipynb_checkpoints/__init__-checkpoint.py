84b0491341203b2592e7bb8bc0e934cddbb948f1
Vincent van de Kerkhof <v.vandekerkhof@student.tudelft.nl>
2019-05-15 23:05:50 +0200

    removed files

--- QMCP/functions/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,8 +0,0 @@
-from .metropolis_algorithm import metropolis
-from .minimal_energy_finder import (derivative_E, 
-                                    minimization_alpha, 
-                                    expectation_value)
-from .error_calculation import bootstrap
-from .energy_and_variance import vmc
-from .parallel_minimal_energy_finder import parallel_minimization_alpha
-
--- QMCP/harmonic_oscillator/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,1 +0,0 @@
-from .oscillator import Oscillator
